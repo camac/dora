15a99d5e5c6f9a1647943d4b2e277002b29e100d
Cameron Gregor <camerongregor@gmail.com>
2013-12-11 23:21:19 +1100

    First attempt at the wrapper script to handle non well formed xml

---
+++ doraFilter.pl
@@ -0,0 +1,40 @@
+#!/bin/perl
+#
+## Steps to success
+#
+# Read STDIN to a variable
+
+my $binDir  = "c:/Users/cgregor/bin";
+my $xslFile = "d:/DominoGit/dorabadxml/xsl/DXLClean.xsl";
+
+my $contents;
+
+while (<>) {
+  $contents .= $_; 
+}
+open(LINT, "| $binDir/xmllint --noout --nowarning - 2>/dev/null") || die "Could not execute the xmllint program\n";
+
+print LINT $contents;
+
+if (close(LINT)) {
+
+  print "Went through the linter ok\n";
+
+  open(XSLTPROC, "| $binDir/xsltproc $xslFile -") || die "Could not execute the xsltproc program\n";
+
+  print XSLTPROC $contents;
+
+  close(XSLTPROC);
+
+} else {
+
+  print "Linter wasn't happy\n";
+  print $contents;
+
+}
+
+# Check error code of STDIN through xmllint
+#
+# if ok, run through xlstproc
+#
+# if not ok, just print contents out
